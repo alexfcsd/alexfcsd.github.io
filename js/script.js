55fec2432bfb0a81162e124ff71647430d78337d
Alexandr Nozdrin <alexbestfor@gmail.com>
2016-04-28 03:39:10 +0300

    Com

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+
+jQuery(document).ready ( function()
+{
+  		jQuery('.draggable').draggable();
+});
+
+	//jQuery('li').remove();z
+	//jQuery('body').append('<a href="http://new.vk.com/aghym1">Моя страница ВК</a>');
+	
+	//jQuery('h3').remove();
+	
+	//var div_clon;
+	
+	//div_clon=jQuery('.part1').clone();
+	
+	//jQuery('body').append(div_clon);
+		
+		
+		
+	
+	
+	//jQuery('input').on('change', function(){
+	//	var value1,value2;
+	
+	//	value1=jQuery('#val1').val();
+		//value2=jQuery('#val2').val();
+	//	jQuery('.part0').html(value1);
+		//alert(Math.sqrt(Math.pow(parseFloat(value1),2)+Math.pow(parseFloat(value2),2)));
+	//});
+	
+//});
+
+//<div class="container">
+ //<div class="row"> 
+  //<br>
+  //<h1>Name of the page</h1>
+  //<p><br>
+//    <a href="#" class="btn btn-default">Button</a>
+    //<a href="#" class="btn btn-primary">Button</a>
+    ///<a href="#" class="btn btn-success">Button</a>
+    //<a href="#" class="btn btn-warning">Button</a>
+    //<a href="#" class="btn btn-info">Button</a>
+    //<a href="#" class="btn btn-danger">Button</a>
+  //</p>
+  //</div>
+//</div>-->
