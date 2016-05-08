f9fbc00bbe00f467d9478aaca7154b2220b0b536
Alexandr Nozdrin <alexbestfor@gmail.com>
2016-05-08 23:52:40 +0300

    cosmet

--- js/script.js
+++ js/script.js
@@ -8,10 +8,6 @@ jQuery(document).ready( function()
     	function (){jQuery(".fa-pencil").removeClass('fa-spin')}
     	);
 
-	
-
-
-  		
     
     jQuery(function(){
         jQuery('[data-toggle="popover"]').popover();
@@ -39,30 +35,3 @@ jQuery(document).ready( function()
 		
 		
 		
-	
-	
-	//jQuery('input').on('change', function(){
-	//	var value1,value2;
-	
-	//	value1=jQuery('#val1').val();
-		//value2=jQuery('#val2').val();
-	//	jQuery('.part0').html(value1);
-		//alert(Math.sqrt(Math.pow(parseFloat(value1),2)+Math.pow(parseFloat(value2),2)));
-	//});
-	
-//});
-
-//<div class="container">
- //<div class="row"> 
-  //<br>
-  //<h1>Name of the page</h1>
-  //<p><br>
-//    <a href="#" class="btn btn-default">Button</a>
-    //<a href="#" class="btn btn-primary">Button</a>
-    ///<a href="#" class="btn btn-success">Button</a>
-    //<a href="#" class="btn btn-warning">Button</a>
-    //<a href="#" class="btn btn-info">Button</a>
-    //<a href="#" class="btn btn-danger">Button</a>
-  //</p>
-  //</div>
-//</div>-->
